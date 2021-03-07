d130941079f96cc1e3a77c366dba9d3d4f186301
OlgaParankina <p.olga@binebase.com>
2021-03-07 18:21:46 +0200

    Applied ui kit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,72 @@
-import React from 'react'
-import scrollTo from 'gatsby-plugin-smoothscroll';
-import logo from '../images/logo.svg'
+import React, { useState } from "react";
+import scrollTo from "gatsby-plugin-smoothscroll";
+import logo from "../images/logo.svg";
 
-const NavLinks = [{
-  path: '#services',
-  name: 'Services'
-},
-{
-  path: '#process',
-  name: 'Our Process'
-},
-{
-  path: '#team',
-  name: 'Team'
-},
-{
-  path: '#about-us',
-  name: 'About Us'
-},
-{
-  path: '#contact-us',
-  name: 'Contact Us'
-},]
+const NavLinks = [
+  {
+    path: "#services",
+    name: "Services",
+  },
+  {
+    path: "#process",
+    name: "Our Process",
+  },
+  {
+    path: "#team",
+    name: "Team",
+  },
+  {
+    path: "#about-us",
+    name: "About Us",
+  },
+  {
+    path: "#contact-us",
+    name: "Contact Us",
+  },
+];
 
 const HeaderNavLink = ({ path, name }) => (
-  <button onClick={() => { scrollTo(path) }} className="header__link">
+  <button
+    onClick={() => {
+      scrollTo(path);
+    }}
+    className="header__link"
+  >
     {name}
   </button>
-)
+);
 
 const Header = () => {
+  const [navBarActiveClass, setNavBarActiveClass] = useState("");
+
   return (
     <header className="header">
-      <div className="container">
-        <div className="header__logo">
-          <img src={logo} alt="logo"/>
-        </div>
-        <div className="header__nav">
-          { NavLinks.map(({ path, name }) => 
-              <HeaderNavLink key={path} path={path} name={name} />
-            )
-          }
-        </div>
+      <div className="header__logo">
+        <img src={logo} alt="logo" />
       </div>
+      <nav
+        className={`navbar ${navBarActiveClass}`}
+        role="navigation"
+        aria-label="main-navigation"
+      >
+        <div className="container">
+          <div
+            className={`navbar-burger ${navBarActiveClass}`}
+            data-target="navMenu"
+            role="button"
+            tabIndex={0}
+            aria-label="navbar-button"
+            onClick={() => setNavBarActiveClass("is-active")}
+            onKeyPress={() => setNavBarActiveClass("is-active")}
+          >
+            {NavLinks.map(({ path, name }) => (
+              <HeaderNavLink key={path} path={path} name={name} />
+            ))}
+          </div>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
